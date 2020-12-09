235fe7bdf4a8f91c613634a46229139eb71e33b5
Dynaste <maximemary.pro@gmail.com>
2020-12-09 09:59:20 +0100

    Done

--- ex-one/src/App.js
+++ ex-one/src/App.js
@@ -1,70 +1,20 @@
-import { Component } from "react";
-import AppartmentsList from "./components/AppartmentsList";
-import FilterList from "./components/FilterList";
+import React, { Component } from "react";
 import SearchBar from "./components/SearchBar";
 
-const appartments = [
-  {
-    id: 1,
-    title: "Large Architect flat - Hypercentral 70m2",
-    price: 45,
-    rating: 4,
-    image:
-      "https://a0.muscache.com/im/pictures/38626db5-6cff-41cc-9120-c4817375efb8.jpg?im_w=720",
-    isAvailable: true,
-  },
-  {
-    id: 2,
-    title: "Studio Luxe - Champs Elysées",
-    price: 55,
-    rating: 5,
-    image:
-      "https://a0.muscache.com/im/pictures/99953686/da93850b_original.jpg?im_w=720",
-    isAvailable: false,
-  },
-  {
-    id: 3,
-    title: "Studio de caractère, Bastille",
-    price: 36,
-    rating: 3,
-    image:
-      "https://a0.muscache.com/im/pictures/80093344/597801ae_original.jpg?im_w=720",
-    isAvailable: true,
-  },
-  {
-    id: 4,
-    title: "Beau studio à 100m du Moulin Rouge",
-    price: 25,
-    rating: 1,
-    image:
-      "https://a0.muscache.com/im/pictures/ae245273-6819-44c4-af6b-7a272d54ff14.jpg?im_w=720",
-    isAvailable: false,
-  },
-];
-const appartmentsAvalaible = [];
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isClick: true,
+      isLoading: true,
       searchContent: "",
+      resultData: null,
+      resultDataWeek: null,
+      errMsg: null,
     };
-
-    this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      isClick: !this.state.isClick,
-    });
-    appartmentsAvalaible.length = 0;
-    appartments.forEach((appartment) => {
-      if (appartment.isAvailable) {
-        appartmentsAvalaible.push(appartment);
-      }
-    });
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange(e) {
@@ -73,30 +23,97 @@ class App extends Component {
     });
   }
 
+  async handleClick() {
+    await this.setState({
+      errMsg: null,
+    });
+    const apiKey = "e918e2b54599b76954a8561c61ef95a7";
+    const cityName = this.state.searchContent;
+    try {
+      const result = await axios.get(
+        // `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${cityName}&appid=${apiKey}`
+        `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`
+      );
+      console.log(result.data);
+      await this.setState({
+        resultData: result.data,
+        resultDataWeek: result.data.list.filter((el) =>
+          el.dt_txt.includes("12:00:00")
+        ),
+        isLoading: false,
+      });
+    } catch (err) {
+      console.log(err);
+      await this.setState({
+        errMsg: "Insert valid city",
+        resultData: null,
+        resultDataWeek: null
+      });
+    }
+  }
+
   render() {
-    const appartmentsFiltered = appartments.filter((appartment) =>
-      appartment.title.toLowerCase().includes(this.state.searchContent)
-    );
+    const {
+      searchContent,
+      resultData,
+      isLoading,
+      resultDataWeek,
+      errMsg,
+    } = this.state;
+    const today = new Date().getDay();
+    const weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
     return (
       <div>
-        <FilterList
-          isClick={this.state.isClick}
+        <SearchBar
+          searchContent={searchContent}
+          handleChange={this.handleChange}
           handleClick={this.handleClick}
         />
-        <SearchBar
-          searchContent={this.searchContent}
-          handleChange={this.handleChange}
-        />
-        {this.state.isClick && !this.state.searchContent && (
-          <AppartmentsList appartments={appartments} />
+
+        {resultData && (
+          <div>
+            <h2>City: {resultData.city.name}</h2>
+            <h4>
+              Date: {new Date().toLocaleString().slice(0, 10).replace(/-/g, "")}
+            </h4>
+            <p>Humidity today : {resultData.list[0].main.humidity}%</p>
+            <p>Weather : {resultData.list[0].weather[0].description}</p>
+            <h5>Next 5 days temps :</h5>
+
+            <p>
+              {weekday[today]} : {resultDataWeek[0].main.temp} °C
+            </p>
+            <p>
+              {weekday[today + 1]} : {resultDataWeek[1].main.temp} °C
+            </p>
+            <p>
+              {weekday[today + 2]} : {resultDataWeek[2].main.temp} °C
+            </p>
+            <p>
+              {weekday[today + 3]} : {resultDataWeek[3].main.temp} °C
+            </p>
+            <p>
+              {weekday[today + 4]} : {resultDataWeek[4].main.temp} °C
+            </p>
+          </div>
         )}
-        {!this.state.isClick && !this.state.searchContent && (
-          <AppartmentsList appartments={appartmentsAvalaible} />
+
+        {!resultData && (
+          <>
+            <p>Search one city</p>
+          </>
         )}
-        {this.state.searchContent && (
-          <AppartmentsList appartments={appartmentsFiltered} />
-        )}
-        <p>{this.state.searchContent}</p>
+
+        {errMsg && <h2>{errMsg}</h2>}
       </div>
     );
   }
--- ex-one/src/components/SearchBar.js
+++ ex-one/src/components/SearchBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 
-const Searchbar = ({searchContent, handleChange}) => {
+const Searchbar = ({searchContent, handleChange, handleClick}) => {
 
     return (
         <>
             <input style={{marginLeft: 20}} type="text" value={searchContent} onChange={handleChange} />
+            <button onClick={handleClick}>search</button>
         </>
     )
 };
